f5ee9afc07d56d5b5f9bd034f1ebaff059a9a130
Afreda323 <anthonyfreda323@gmail.com>
2018-03-28 21:41:02 -0400

    Rocket can thrust and rotate

---
+++ Assets/Rocket.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour {
+
+    Rigidbody rocketBody;
+
+	// Use this for initialization
+	void Start () {
+        rocketBody = GetComponent<Rigidbody>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        HandleKey();
+	}
+
+    private void HandleKey () {
+        // Directions
+        if (Input.GetKey(KeyCode.A)) {
+            transform.Rotate(Vector3.forward);
+        } else if (Input.GetKey(KeyCode.D)) {
+            transform.Rotate(-Vector3.forward);
+        } 
+
+        // Thrust
+        if (Input.GetKey(KeyCode.Space)) {
+            rocketBody.AddRelativeForce(Vector3.up);
+        }
+    }
+}
